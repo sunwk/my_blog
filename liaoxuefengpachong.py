bbecec62fad1baedbd074fd8ab949b235f7e32a4
sunwk <546602667@qq.com>
2016-09-03 15:25:40 +0800

    解决模态框问题
    无法弹出模态框：没引用bootstra的js文件
    
    弹出模态框全是灰色：bootstrap的css文件必须是3.3.0，之前用的是3.1.0版本太旧。而且爬csn文件到本地有一些问题，只能在页面里引用cdn了

--- liaoxuefengpachong.py
+++ liaoxuefengpachong.py
@@ -7,6 +7,6 @@ url_2 = prifix + '/static/themes/default/css/highlight.css'
 url_3 = prifix + '/static/themes/default/css/itranswarp.css'
 
 
-with open('./static/css/blogcss2.css', 'w+', encoding='utf-8') as f:
-    t1 = requests.get('http://aljun.me/static/mystyle.css').text
+with open('./static/css/bootstrap3.3.min.css', 'w+', encoding='utf-8') as f:
+    t1 = requests.get('http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css').text
     f.write(t1)
